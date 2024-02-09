a67932c70fb729aaf2b7dfa623cd3c5589f98398
RoastChestnut <chestnut.roasted@outlook.com>
2024-02-10 00:40:04 +0800

    ✅ test: 修改了测试代码

--- test/neuron_model_test.exs
+++ test/neuron_model_test.exs
@@ -1,4 +1,7 @@
 defmodule NeuronModelTest do
+  @moduledoc """
+  Test at model level.
+  """
   use ExUnit.Case
   # doctest Glowworm.Models
 
@@ -17,7 +20,7 @@ defmodule NeuronModelTest do
       %I.RunnerState{counter: 0}
     )
 
-    assert %I.RunnerState{counter: 1, event: nil} = runner
+    assert %I.RunnerState{counter: 1, event: nil} == runner
   end
 
   test "action_potential" do
@@ -28,7 +31,18 @@ defmodule NeuronModelTest do
       %I.RunnerState{counter: 0}
     )
 
-    assert %I.RunnerState{counter: 1, event: nil} = runner
+    assert %I.RunnerState{counter: 1, event: :pulse} == runner
+  end
+
+  test "counter reset" do
+    {next_s, runner} = I.nextstep(
+      izh_param(),
+      %I.NeuronState{potential: -65.0, recovery: -9.2},
+      %I.InputState{current: 5.0},
+      %I.RunnerState{counter: 255}
+    )
+
+    assert runner.counter == 0
   end
 
   ## LIF Model
