1232ca5275b19bfbbda1153e945a62ddbb874e49
RoastChestnut <chestnut.roasted@outlook.com>
2024-02-10 22:43:25 +0800

    📝 docs: 更新了用例

--- example/neuron_runner/demo.py
+++ example/neuron_runner/demo.py
@@ -8,9 +8,21 @@ file = (Path.cwd() / "example/neuron_runner/r_temp.csv")
 df = pd.read_csv(file)
 # cycles / timestep, timestep
 t = np.arange(0, (256*256+1)/100, 0.01)
-plt.plot(t, df['v'])
+
+# plt.figure(1)
+f, axarr = plt.subplots(2, sharex=True)
+f.suptitle('Chattering Cell in Izhikevich Model')
+
+axarr[0].plot(t, df['v'], color='#1f77b4')
+axarr[0].set_title('potential(v)', color='gray')
+
+axarr[1].plot(t.copy(), df['u'], color='#ffaa7f')
+axarr[1].set_title('recovery(u)', color='gray')
+
+# plt.legend('potential', 'recovery')
+plt.xlim(-5.0, 660.0)
 plt.show()
 
 if __name__ == '__main__':
-    # TODO: Add timestep.
+    # TODO: Get timestep in args from elixir.
     pass
