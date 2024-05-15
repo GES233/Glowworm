eb9cacb2e3d861b89202dacf2c2d6d3c984fdf46
Chestnut <chestnut.roasted@outlook.com>
2024-05-15 19:54:29 +0800

    添加了用于配置模型参数的部分代码

--- lib/glowworm/soma_runner.ex
+++ lib/glowworm/soma_runner.ex
@@ -8,26 +8,72 @@ defmodule Glowworm.SomaRunner do
   * Adjust timestep
   """
   use Task
-  @default_model Glowworm.Models.Izhikevich
-  # TODO: fetch module from config.
 
-  defp get_module(opts), do: Keyword.get(opts, :runner_module, @default_model)
+  ## Configuration.
+  @type conf :: %{
+          modules: %{
+            param: atom(),
+            input: atom(),
+            state: atom(),
+            runner: atom()
+          },
+          model: atom(),
+          param: map() | struct(),
+        }
+
+  @runner_model :soma_runner_model
+  @model_params :soma_runner_module_param
+  @model_modules [:param, :input, :state, :runner]
+  @default_model Glowworm.Models.Izhikevich
+  @default_modules %{
+    param: Glowworm.Models.Izhikevich.Param,
+    input: Glowworm.Models.Izhikevich.InputState,
+    state: Glowworm.Models.Izhikevich.NeuronState,
+    runner: Glowworm.SomaRunner.RunnerState
+  }
+  # @steps_number_per_frame 0xff  # Includes 0.
+
+  defp default_module(), do: @default_modules |> Enum.map(fn {k, v} -> {k, v} end)
+
+  defp get_module(_conf_map = %{modules: modules}) , do: modules
+  defp get_module(opts) do
+    @model_modules
+    |> Enum.map(fn field -> {field, Keyword.get(opts ++ default_module(), field)} end)
+    |> Map.new()
+  end
+
+  defp get_model(_conf_map = %{model: model}) , do: model
+  defp get_model(opts), do: Keyword.get(opts, @runner_model, @default_model)
+
+  defp get_param(_conf_map = %{params: params}) , do: params
+  defp get_param(opts), do: Keyword.get(opts, @model_params)
 
   def start_link(args), do: Task.start_link(__MODULE__, :run, args)
 
   def run({opts}) do
-    _runner_module = get_module(opts)
+    ## Load runner.
+    _runner_module_name = get_module(opts)
+    _runner_model = get_model(opts)
+    _runner_params = get_param(opts)
   end
 
-  # @frame_count 0xff
-  # defp do_run(), do: nil
-  # TODO: Implement runner.
-  # - [ ] continous calculate with recursion
-  #   - [x] prototype in example
-  #   - [ ] here
-  # - [ ] when counter = 0 =>
+  # Simulation.
+
+  # get_current(), do: nil
+  # get_neuron_state(), do: nil
+  # if has not state(e.g. when initialize) => ...
+  # else => when currrent state
+
+  # Wrap `Models.next_step/4`
+  def do_single_step() do
+    # ...
+  end
+
+  # Implemented runner prototype in `Demo`.
+  # - continous calculate with recursion
+  # - when counter = 0 =>
   #   do_some_stuff(send data to Neuron)
-  # - [ ] when pulse =>
+  # - when pulse =>
   #   do_some_stuff(send event to Neuron)
 end
 
