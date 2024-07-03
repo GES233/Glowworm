5abf2bda07d7a3e9f7b8399a79dd5a5004f11bcb
RoastChestnut <chestnut.roasted@outlook.com>
2024-07-03 21:48:24 +0800

    【部分提交】完善了 `SyncRunner`

--- lib/glowworm/sync_runner.ex
+++ lib/glowworm/sync_runner.ex
@@ -3,4 +3,32 @@ defmodule Glowworm.Neuron.SyncRunner do
   Intergrate all runners and running it
   in a synchrone method.
   """
+  use Task
+
+  #alias Glowworm.SomaRunner.RunnerState, as: SomaState
+  #alias Glowworm.SynapseRunner.RunnerState, as: SynState
+
+  def start_link(args) do
+    Task.start_link(__MODULE__, :run, [args])
+  end
+
+  defp get_soma(), do: nil
+  defp get_syns(), do: nil
+
+  defp prelude() do
+    _soma = get_soma()
+    _syns = get_syns()
+
+    %{}
+  end
+
+  def execute_frame() do
+    # ...
+
+    _ = prelude()
+  end
+end
+
+defmodule Glowworm.Neuron.SyncRunner.Conf do
+  # Value container while running.
 end
