425f2a2be7a938506dd18b050f9ee98e688fbb1b
RoastChestnut <chestnut.roasted@outlook.com>
2024-08-25 20:16:26 +0800

    对应的添加 `SynapseRunner` 的部分函数

--- lib/glowworm/synapse_runner.ex
+++ lib/glowworm/synapse_runner.ex
@@ -1,7 +1,42 @@
 defmodule Glowworm.SynapseRunner do
   # alias :gen_statem, as: GenStateM
+  alias GLowworm.Models, as: M
+  alias Glowworm.SynapseRunner, as: S
 
   # @behaviour GenStateM
+
+  @type state :: :idle | :running
+  @type container :: {M.param(), M.state(), M.input(), S.RunnerState.t()}
+  @type machine_state :: %{
+          state: state(),
+          container: container(),
+          model: atom() | module(),
+          conn: %{event: pid() | nil, inspect: pid() | nil}
+        }
+
+  ## Inner Functions
+
+  @spec do_next_step(machine_state()) :: machine_state()
+  def do_next_step(state) do
+    {next_state, next_runner_state} = apply(state[:model], :nextstep, state[:container])
+    {param, _state, input, _runner_state} = state[:container]
+
+    %{state | container: {param, next_state, input, next_runner_state}}
+  end
+
+  @spec do_receive_pulse(machine_state(), any(), fun()) :: machine_state()
+  def do_receive_pulse(state, input, convert_func) do
+    {param, state, _prev_input, runner_state} = state[:container]
+
+    %{state | container: {param, state, convert_func.(input), runner_state}}
+  end
+
+  @spec do_update_runner_state(machine_state(), S.RunnerState.t()) :: machine_state()
+  def do_update_runner_state(state, new_runner_state) do
+    {param, state, input, _runner_state} = state[:container]
+
+    %{state | container: {param, state, input, new_runner_state}}
+  end
 end
 
 defmodule Glowworm.SynapseRunner.RunnerState do
